81efd7e9b853de6d6eeced339f3159970db9b2e7
Marcus Lankenau <marcus.lankenau@gmail.com>
2016-04-15 21:41:57 +0200

    ai works (but stupid)

---
+++ base_client.rb
@@ -0,0 +1,150 @@
+require 'rubygems'
+require 'websocket-client-simple'
+require 'json'
+require 'ostruct'
+require 'byebug'
+
+class Planet < OpenStruct
+  def distance(planet)
+    Math.sqrt((planet.x - x)*(planet.x - x) + (planet.y - y)*(planet.y - y))
+  end
+end
+
+class BaseClient
+  LOBBY = "games:lobby"
+
+  attr_accessor :player_num
+  attr_accessor :planets
+
+  def initialize
+    @mode = :lobby
+
+    puts "init"
+    @ws = WebSocket::Client::Simple.connect('ws://ant-attack.com/socket/websocket')
+    puts "got ws #{@ws}"
+    me = self
+    @ws.on :message do |msg|
+      begin
+        msg = JSON.parse(msg.data)
+        event = msg['event']
+        payload = msg['payload']
+        me.send("on_#{event}".to_sym, payload)
+      rescue NoMethodError => e
+        puts "missing method: #{e}"
+      rescue => e
+        puts "#{e.class} #{e.message}, #{e.backtrace[0..100]}"
+      end
+    end
+
+    @ws.on :open do
+      puts "open event"
+    end
+
+    @ws.on :close do |e|
+      p e
+      exit 1
+    end
+
+    @ws.on :error do |e|
+      p e
+    end
+  end
+
+  def name
+    "AI Player"
+  end
+
+  def login
+    ws_send(LOBBY, 'phx_join', {name: name})
+  end
+
+  def ws_send(topic, event, payload)
+    msg = {topic: topic, event: event, payload: payload, ref: next_ref.to_s}
+    @ws.send(msg.to_json)
+  end
+
+  def shoot(from, to)
+    ws_send(@game_channel, 'shoot', {from: from, to: to})
+  end
+
+  def on_phx_reply(payload)
+  end
+
+  def on_challenge(payload)
+    if payload['to'] == name
+      ws_send(LOBBY, 'accept_challenge', from: payload['from'], to: name)
+    end
+  end
+
+  def on_players_update(payload)
+  end
+
+  def on_enter_game(payload)
+    if payload['name'] == name
+      @map = payload['map']
+      @planets = []
+      @player_num = payload['player_num'].to_i
+      @game_channel = "games:#{payload['game_pid']}"
+      ws_send(LOBBY, 'phx_leave', {})
+      ws_send(@game_channel, 'phx_join', {player_num: payload['player_num']})
+      @mode = :transient
+    end
+  end
+
+  def on_update_planets_score(payload)
+    @planets = payload['planets'].map { |h| Planet.new(h)}
+  end
+
+  def on_update_planets(payload)
+    @planets = payload['planets'].map { |h| Planet.new(h)}
+  end
+
+  def on_game_over(payload)
+    ws_send(@game_channel, 'phx_leave', {})
+    login
+    @mode = :lobby
+  end
+
+  def on_send_ships(payload)
+  end
+
+  def wait_for_challenge
+    @mode = :lobby
+    while @mode == :lobby do
+      sleep 1
+      ws_send(LOBBY, 'lobby_alive', {from: name})
+    end
+  end
+
+  def game_loop
+    @mode = :game
+    while @mode == :game do
+      play if planets.size > 0
+      sleep(1)
+    end
+  end
+
+  def main_loop
+    loop do
+      if @ws.open?
+        login
+        sleep 1
+        game_active = true
+        loop do
+          wait_for_challenge
+          game_loop
+        end
+      else
+        sleep 1
+      end
+    end
+  end
+
+  private
+
+  def next_ref
+    @next_ref ||= 0
+    @next_ref += 1
+  end
+
+end
--- client.rb
+++ client.rb
@@ -1,113 +1,24 @@
 require 'rubygems'
 require 'websocket-client-simple'
 require 'json'
+require './base_client'
 
-
-class Client
+class Client < BaseClient
   LOBBY = "games:lobby"
 
-  def initialize
-    puts "init"
-    @ws = WebSocket::Client::Simple.connect('ws://localhost:4000/socket/websocket')
-    puts "got ws #{@ws}"
-    me = self
-    @ws.on :message do |msg|
-      begin
-        msg = JSON.parse(msg.data)
-        event = msg['event']
-        payload = msg['payload']
-        if event == 'players_update'
-          me.on_player_update(payload)
-        elsif event == 'challenge'
-          if payload['to'] == me.name
-            me.on_challenge(payload['from'])
-          else
-            puts msg
-          end
-        elsif event == 'enter_game'
-          if payload['name'] == me.name
-            me.on_enter_game(payload)
-          end
-        else
-          puts "unknown event #{msg['event']}"
-        end
-      rescue => e
-        puts "#{e.message}, #{e.backtrace}"
-      end
-    end
-
-    @ws.on :open do
-      puts "open event"
-    end
-
-    @ws.on :close do |e|
-      p e
-      exit 1
-    end
-
-    @ws.on :error do |e|
-      p e
-    end
-  end
-
   def name
-    "AI Player"
+    'AI Player'
   end
 
-  def login
-    puts "login open: #{@connected}"
-    ws_send(LOBBY, 'phx_join', {name: name})
-  end
+  def play
+    neutrals = planets.select { |p| p.player != player_num }
+    my = planets.select { |p| p.player == player_num}
 
-  def ws_send(topic, event, payload)
-    msg = {topic: topic, event: event, payload: payload, ref: next_ref.to_s}
-    @ws.send(msg.to_json)
-  end
-
-  def on_challenge(from)
-    puts "received challenge from #{from}"
-    ws_send(LOBBY, 'accept_challenge', from: from, to: name)
-  end
-
-  def on_player_update(payload)
-    puts "got player update #{payload['players']}"
-  end
-
-  def on_enter_game(payload)
-    @map = payload['map']
-    puts "enter game: #{payload}"
-    ws_send(LOBBY, 'phx_leave', {})
-    ws_send("game:#{@map['game_pid']}", 'phx_join', {player_num: @map['player_num']})
-  end
-
-  def wait_for_challenge
-    loop do
-      sleep 1
-      ws_send(LOBBY, 'lobby_alive', {from: name})
+    my.each do |src|
+      sorted = neutrals.sort { |x, y| src.distance(x) <=> src.distance(y) }
+      shoot(src.idx, sorted.first.idx)
     end
   end
-
-  def main_loop
-    loop do
-      if @ws.open?
-        login
-        sleep 1
-        game_active = true
-        wait_for_challenge
-      else
-        sleep 1
-        p "waiting for connection"
-      end
-    end
-  end
-
-  private
-
-  def next_ref
-    @next_ref ||= 0
-    @next_ref += 1
-  end
-
 end
 
 client = Client.new
